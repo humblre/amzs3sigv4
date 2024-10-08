1881cd4c8b9f6941305b6c73b275cbfed580b80c
humblre <busypostbox@gmail.com>
2024-10-08 15:00:17 +0900

    WIP

--- amzs3sigv4.py
+++ amzs3sigv4.py
@@ -6,43 +6,54 @@ from urllib.parse import quote, urlparse
 from datetime import datetime
 import hmac
 import socket
+import ssl
+
+"""
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric.utils import (
+    encode_dss_signature, decode_dss_signature)
+from cryptography.hazmat.backends import default_backend
+"""
 
 
 class SimpleTCPClient():
-    def __init__(self, host: str, port: int):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, host: str, port: int, use_ssl: bool = False):
+        self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._host = host
         self._port = port
 
-    def _send(self, payload: bytes):
-        self.client_socket.connect((self._host, self._port))
+        if port == 443 or use_ssl:
+            context = ssl.create_default_context()
+            self._client_socket = context.wrap_socket(
+                self._client_socket, server_hostname=self._host)
 
-        self.client_socket.sendall(payload)
+    def _send(self, payload: bytes):
+        self._client_socket.connect((self._host, self._port))
+
+        self._client_socket.sendall(payload)
 
         parts = []
         while True:
-            parts.append(self.client_socket.recv(1024<<10))
+            parts.append(self._client_socket.recv(1024 << 10))
             if not parts[len(parts) - 1]:
                 break
-        self.client_socket.close()
+        self._client_socket.close()
         parts.pop()  # Consume a last empty part
         return b''.join(parts)
 
-    def __call__(self, payload: bytes):
+    def __call__(self, payload: bytes) -> bytes:
         return self._send(payload)
 
 
 class SimpleRESTClient(SimpleTCPClient):
-    def _send(self, payload: bytes):
-        raise NotImplementedError()
-
     def __init__(self, url: str):
         parsed_url = urlparse(url)
         parsed_netloc = parsed_url.netloc.split(':')
 
         if len(parsed_netloc) == 2:
             port = parsed_netloc[1]
-        elif parsed_url.scheme in ('http', '') :
+        elif parsed_url.scheme in ('http', ''):
             port = 80
         elif parsed_url.scheme == 'https':
             port = 443
@@ -56,7 +67,8 @@ class SimpleRESTClient(SimpleTCPClient):
 
         SimpleTCPClient.__init__(self, host, port)
 
-    def __call__(self, method: str='GET', headers: dict=None, body: bytes=b''):
+    def __call__(self, method: str = 'GET', headers: dict = None,
+                 body: bytes = b'') -> bytes:
         payload = '\n'.join(
             [
                 f'{method} {self._uri} HTTP/1.1',
@@ -86,15 +98,22 @@ class SimpleRESTClient(SimpleTCPClient):
         return self._send(payload)
 
 
-class AuthorizationHeader():
+class StreamingUpload(SimpleRESTClient):
+    def __call__(self, body: bytes, headers: dict = None) -> bytes:
+        pass
+
+
+class AuthHeader():
     def __init__(self, http_verb: str, uri: str, algorithm: str, region: str,
-                 secret_access_key: str, headers: dict = None,
+                 access_key: str, secret_access_key: str, headers: dict = None,
                  query_strings: dict = None, has_trailer: bool = False):
+        self._access_key = access_key
         self._secret_access_key = secret_access_key
         self._region = region
         self._algorithm = algorithm
         self._has_trailer = has_trailer
-        headers = headers if headers else {}
+        self._utcnow = datetime.utcnow()
+        self.headers = headers
         query_strings = query_strings if query_strings else {}
 
         self._canonical_request = (
@@ -111,11 +130,28 @@ class AuthorizationHeader():
                     # Canonical Headers
                     '\n'.join(
                         [f'{k.lower()}:{v.strip()}'
-                         for k, v in headers.items()]),
+                         for k, v in self._headers.items()]),
                     # Signed Headers
-                    ';'.join([k.lower() for k in headers]),
+                    ';'.join([list(self.headers)]),
                     # x-amz-content-sha256
                     self.x_amz_content_sha256]))
+
+    @property
+    def headers(self):
+        return self._headers
+
+    @headers.setter
+    def headers(self, headers: dict):
+        self._headers = {k.lower: v for k, v in headers.items()}
+        if 'x-amz-date' not in self._headers:
+            self._headers['x-amz_date'] = self.amz_date
+        if 'x-amz-content-sha256' not in self._headers:
+            self._headers['x-amz-content-sha256'] = self.x_amz_content_sha256
+        sorted_keys = sorted(self._headers)
+        self._headers = {
+            k: (
+                self._headers[k].strip() if self._headers[k] else '')
+            for k in sorted_keys}
 
     @property
     def x_amz_content_sha256(self):
@@ -123,14 +159,18 @@ class AuthorizationHeader():
                 '-TRAILER' if self._has_trailer else '')
 
     @property
-    def timestamp(self):
-        return datetime.now().isoformat()  # yyyymmddThhmmssZ
+    def date(self):
+        return self._utcnow.strftime('%Y%m%d'),
+
+    @property
+    def amz_date(self):
+        return self._utcnow.isoformat()  # yyyymmddThhmmssZ
 
     @property
     def scope(self):
         return '/'.join(
             [
-                datetime.now().strftime('%Y%m%d'),
+                self.date,
                 self._region,
                 's3',
                 'aws4_request'])
@@ -140,30 +180,36 @@ class AuthorizationHeader():
         return '\n'.join(
             [
                 self._algorithm,
-                self.timestamp,
+                self.amz_date,
                 self.scope,
                 sha256(self._canonical_request.encode('utf8')).hexdigest()])
 
     @property
     def signing_key(self):
         date_key = hmac.new(b'AWS4' + self._secret_access_key.encode('utf8'),
-                            datetime.now().strftime('%Y%m%d').encode('utf8'),
-                           sha256)
+                            self.date.encode('utf8'),
+                            sha256)
         date_region_key = hmac.new(date_key.digest(),
                                    self._region.encode('utf8'),
-                                  sha256)
+                                   sha256)
         date_region_service_key = hmac.new(date_region_key.digest(),
                                            b's3',
-                                          sha256)
+                                           sha256)
         return hmac.new(date_region_service_key.digest(),
                         b'aws4_request',
-                       sha256)
+                        sha256)
 
     @property
     def seed_signature(self):
         return hmac.new(self.signing_key.digest(),
                         self.string_to_sign.encode('utf8'),
-                       sha256)
+                        sha256)
 
-    def __str__(self):
-        return self.seed_signature.hexdigest()
+    @property
+    def Authorization(self):
+        return (
+            f'{self._algorithm} '
+            f'Credential={self._access_key}/{self.date}/{self._region}/'
+            f's3/aws4_request,'
+            f'SignedHeaders={self._signed_headers}',
+            f'Signature={self.seed_signature.hexdigest()}')
